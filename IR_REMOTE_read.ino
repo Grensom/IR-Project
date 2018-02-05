161a91355c2b6e8d3859a5bc6bcb090d0ad50961
Greg Ensom <36168234+Grensom@users.noreply.github.com>
2018-02-05 14:01:36 -0500

    Add files via upload

---
+++ IR_REMOTE_read.ino
@@ -0,0 +1,37 @@
+//2017.06.22
+
+#include <IRremote.h>
+#include <LiquidCrystal.h>
+
+/* HOW TO WIRE UP LCD SCREEN LEFT TO RIGHT PIN WITH PINS ON TOP SIDE
+ *  
+ *  GND - VCC - POT MIDDLE - 12 - GND - 11 - NONE - NONE - NONE - NONE - 5 - 4 - 9 - 2 - VCC - GND
+ *  
+ *  POT MUST HAVE 5V AND GND ON IT AND CAN ROTATE THE KNOB TO CHANGE LCD CONTRAST
+ */
+ 
+LiquidCrystal lcd(12, 11, 5, 4, 9, 2); //9 instead of 3 (NEED FOR TRANSMITTER)
+
+int receiverpin = 10;
+
+IRrecv irrecv(receiverpin);
+decode_results results;
+
+
+void setup() {
+  lcd.begin(16, 2);
+  pinMode(receiverpin, INPUT);
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+
+void loop() {
+  if (irrecv.decode(&results))
+  {
+    Serial.println(results.value, HEX);
+    irrecv.resume();
+    lcd.clear();
+    lcd.print(results.value, HEX);
+    delay(150);
+  }
+}
---
+++ IR_REMOTE_send.ino
@@ -0,0 +1,36 @@
+
+#include <IRremote.h>
+
+IRsend irsend;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(10000);
+  //Power//
+  irsend.sendNEC(..., 32); //Power Code
+  Serial.println("Power");
+  delay(10000);
+  //Volume Up//
+  for (int i = 0; i < 10; i++) {
+    irsend.sendNEC(..., 32); //Volume Up Code
+    Serial.println("Volume Up");
+    delay(250);
+  }
+  delay(5000);
+  //Volume Down//
+  for (int i = 0; i < 10; i++) {
+    irsend.sendNEC(..., 32); //Volume Down Code
+    Serial.println("Volume Down");
+    delay(250);
+  }
+  delay(5000);
+  //Netflix//
+  irsend.sendNEC(..., 32); //Netflix Code
+  Serial.println("Netflix");
+  delay(10000);
+}
+
+
